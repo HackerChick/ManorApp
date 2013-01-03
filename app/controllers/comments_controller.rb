c914173a304a43daf75d44e18098185f3ef40456
HackerChick <haxrchick@gmail.com>
2013-01-03 17:55:12 -0500

    Finished comments: new comments added to page with ajax, focus given to comment field when you go to add new one, comment box removed when you click return in comment box (either to add new one or if empty it just clears it), and unused actions/files removed from comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,20 +5,8 @@ class CommentsController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
-
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = @item.comments.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @comments }
-    end
-  end
-
-  # GET /comments/1
-  # GET /comments/1.json
+  # GET items/id/comments/1
+  # GET items/id/comments/1.json
   def show
     @comment = Comment.find(params[:id])
 
@@ -28,64 +16,28 @@ class CommentsController < ApplicationController
     end
   end
 
-  # GET /comments/new
-  # GET /comments/new.json
+  # GET items/id/comments/new
   def new
     @comment = Comment.new
 
     respond_to do |format|
-      format.html
       format.js
     end
   end
 
-  # GET /comments/1/edit
-  def edit
-    @comment = Comment.find(params[:id])
-  end
-
-  # POST /comments
-  # POST /comments.json
+  # POST items/id/comments
   def create
     @comment = Comment.new(params[:comment])
-    @comment.datetime = Time.now
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to items_path, notice: 'Comment was successfully created.' }
-        format.json { render json: @comment, status: :created, location: @comment }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if( @comment.text.length > 0 )
+      @comment.datetime = Time.now
+      @comment.save
+    else
+      @comment = nil
     end
-  end
-
-  # PUT /comments/1
-  # PUT /comments/1.json
-  def update
-    @comment = Comment.find(params[:id])
 
     respond_to do |format|
-      if @comment.update_attributes(params[:comment])
-        format.html { redirect_to items_path, notice: 'Comment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comments/1
-  # DELETE /comments/1.json
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to items_path }
-      format.json { head :no_content }
+      format.js
+      #format.html { redirect_to items_path}
     end
   end
 end
