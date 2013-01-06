f5b7ece87273948792fe6c731e1ce9b2b6dec674
HackerChick <haxrchick@gmail.com>
2013-01-06 14:01:39 -0500

    Updated voting to be done via ajax. Fixes voting on item page doesn't return to items listing.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -16,82 +16,23 @@ class VotesController < ApplicationController
     end
   end
 
-  # GET /items/1/votes/1
-  # GET /items/1//votes/1.json
-  def show
-    @vote = Vote.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @vote }
-    end
-  end
-
+  # This is handling the yes/no/clear actions so does new/create/update/destroy
+  # TODO: rename this action to something clearer like "vote"
   # GET /items/1/votes/new
-  # GET /items/1/votes/new.json
   def new
     current_user.units.each do |unit|
       if :choice.nil?
         vote = Vote.find_by_item_id_and_unit_id(@item.id, unit.id)
-        vote.destroy if !vote.nil? # TODO: Move this to destroy!
+        vote.destroy if !vote.nil?
       else
-        vote = Vote.find_or_create_by_item_id_and_unit_id(@item.id, unit.id)
-        vote.update_attributes( :choice => params[:choice] )
+        @vote = Vote.find_or_create_by_item_id_and_unit_id(@item.id, unit.id)
+        @vote.update_attributes( :choice => params[:choice] )
       end
     end
 
     respond_to do |format|
-      format.html { redirect_to items_path, notice: 'Vote was successfully created.' }
-      format.json { render json: @vote }
+      format.js
     end
   end
 
-  # GET /items/1/votes/1/edit
-  def edit
-    @vote = Vote.find(params[:id])
-  end
-
-  # POST /items/1/votes
-  # POST /items/1/votes.json
-  def create
-    @vote = Vote.new(params[:vote])
-
-    respond_to do |format|
-      if @vote.save
-        format.html { redirect_to [@item,@vote], notice: 'Vote was successfully created.' }
-        format.json { render json: @vote, status: :created, location: @vote }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /items/1/votes/1
-  # PUT /items/1/votes/1.json
-  def update
-    @vote = Vote.find(params[:id])
-
-    respond_to do |format|
-      if @vote.update_attributes(params[:vote])
-        format.html { redirect_to [@item,@vote], notice: 'Vote was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /items/1/votes/1
-  # DELETE /items/1/votes/1.json
-  def destroy
-    @vote = Vote.find(params[:id])
-    @vote.destroy
-
-    respond_to do |format|
-      format.html { redirect_to new_url }
-      format.json { head :no_content }
-    end
-  end
 end
